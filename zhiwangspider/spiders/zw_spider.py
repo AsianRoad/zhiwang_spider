31e84b6f14ec00d326946669fd68295859c29eed
AsianRoad <wzc1416353344@outlook.com>
2017-05-19 15:01:07 +0800

    知网爬虫：第二次提交；添加cookie；还未解决跳转

--- zhiwangspider/spiders/zw_spider.py
+++ zhiwangspider/spiders/zw_spider.py
@@ -33,21 +33,50 @@ formdata2=urllib.urlencode(
     }
 )
 
+formdata4 = urllib.urlencode(
+    {
+        'curpage':'17',
+        'RecordsPerPage':'10',
+        'QueryID':'5',
+        'ID':'',
+        'turnpage':'1',
+        'tpagemode':'L',
+        'dbPrefix':'CJFQ',
+        'Fields':'',
+        'DisplayMode':'listmode',
+        'PageName':'ASP.brief_result_aspx'
+    }
+)
+
 # 验证码表单
-ccform = {
-'rurl':'/kns/brief/brief.aspx?curpage=16&RecordsPerPage=10&QueryID=5&ID=&turnpage=1&tpagemode=L&dbPrefix=CJFQ&Fields=&DisplayMode=listmode&PageName=ASP.brief_result_aspx'
+#
+formdata3 = {
+    'rurl':'/kns/brief/brief.aspx?curpage=16&RecordsPerPage=10&QueryID=5&ID=&turnpage=1&tpagemode=L&dbPrefix=CJFQ&Fields=&DisplayMode=listmode&PageName=ASP.brief_result_aspx&sKuaKuID=5',
+    'vericode':''
 }
+
+
 
 class zwspider(Spider):
     name = 'zw'
     allowed_domains = []
     start_urls = ['http://kns.cnki.net/kns/brief/result.aspx?dbprefix=CJFQ']
 
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Connection': 'keep-alive',
+        'Host': 'kns.cnki.net',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Upgrade-Insecure-Requests':'1'
+    }
+
     def __init__(self, query='', time=''):
         # 第一次链接
         # http://kns.cnki.net/kns/request/SearchHandler.ashx?
         self.formdata = urllib.urlencode(
-            {'magazine_value1': '数学学报中文版', 'year_from': 2014, 'year_to': 2016, 'NaviCode': '*', 'ua': '1.21',
+            {'magazine_value1': '数学学报中文版', 'year_from': '2014', 'year_to': '2016', 'NaviCode': '*', 'ua': '1.21',
              'PageName': 'ASP.brief_result_aspx',
              'DbPrefix': 'CJFQ', 'DbCatalog': '中国学术期刊网络出版总库', 'ConfigFile': 'CJFQ.xml', 'db_opt': 'CJFQ',
              'db_value': '中国学术期刊网络出版总库', 'magazine_special1': '=', 'year_type': 'echar', 'his': '0',
@@ -57,6 +86,8 @@ class zwspider(Spider):
     # 处理response，并返回处理的数以及跟进的url,但是在response 没有指定函数的情况下的默认处理方法
     def parse(self, response):
         return Request("http://epub.cnki.net//kns/request/SearchHandler.ashx?"+self.formdata,
+                       headers=self.headers,
+                       meta={'cookiejar':1},
                        callback=self.Search1
                        )
 
@@ -65,13 +96,17 @@ class zwspider(Spider):
         with open(filename, 'wb') as f:
             f.write(response.body)
         return Request("http://epub.cnki.net/kns/brief/brief.aspx?"+formdata1,
+                       headers=self.headers,
+                       meta={'cookiejar':response.meta['cookiejar']},
                        callback=self.Search2
                        )
     def Search2(self,response):
-        filename = 'search1.html'
+        filename = 'search2.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
         return Request("http://epub.cnki.net/kns/brief/brief.aspx?"+formdata2,
+                       headers=self.headers,
+                       meta={'cookiejar':response.meta['cookiejar']},
                        callback=self.Search3)
 
 
@@ -98,10 +133,14 @@ class zwspider(Spider):
             img.save('./cap.png')
             checkcode = distinguish_captcha(img)
             print checkcode
-            ccform['vericode'] = checkcode
-            formdata3 = urllib.urlencode(ccform)
-            yield Request("http://kns.cnki.net/kns/brief/vericode.aspx?"+formdata3,
-                           callback=self.Search3)
+            formdata3['vericode'] = checkcode
+            veriform = urllib.urlencode(formdata3)
+            print veriform
+            yield Request(
+                "http://kns.cnki.net/kns/brief/brief.aspx?" + veriform,
+                headers=self.headers,
+                meta={'cookiejar':response.meta['cookiejar']},
+                callback=self.Search4)
 
         next_pages = response.xpath('//a[@id="Page_next"]/@href').extract_first()
         for tr in response.css('table.GridTableContent').xpath('tr[position()>1]'):
@@ -115,4 +154,16 @@ class zwspider(Spider):
 
         next_url = urljoin('http://epub.cnki.net/kns/brief/brief.aspx',next_pages)
         if next_url:
-            yield  Request(next_url,callback=self.Search3)
+            yield  Request(next_url,
+                           headers=self.headers,
+                           meta={'cookiejar': response.meta['cookiejar']},
+                           callback=self.Search3)
+
+    def Search4(self,response):
+        filename = 'veri.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        return Request("http://kns.cnki.net/kns/brief/brief.aspx?"+formdata4,
+                       headers=self.headers,
+                       meta={'cookiejar':response.meta['cookiejar']},
+                       callback=self.Search3)
